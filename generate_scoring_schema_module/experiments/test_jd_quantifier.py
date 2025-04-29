005b55f84c0ac07105e72371c0662859b2eb4bfe
TheMetaSetter <134222962+TheMetaSetter@users.noreply.github.com>
2025-04-29 21:34:01 +0200

    Add testing script

---
+++ generate_scoring_schema_module/experiments/test_jd_quantifier.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(".."))
+
+from jd_quantifier import gen_and_log_schema_from_jd
+import posixpath
+
+jd_path = posixpath.join("..", "data", "raw", "test_jd", "VEO - JD DIGITAL MARKETING COLLABORATOR 2025 (PART-TIME) .txt")
+google_api_key_path = posixpath.join("..", "secret_keys", "gemini_api_key.txt")
+athina_api_key_path = posixpath.join("..", "secret_keys", "athina_api_key.txt")
+cleanlab_tlm_api_key_path = posixpath.join("..", "secret_keys", "cleanlab_tlm_api_key.txt")
+
+with open(google_api_key_path, "r", encoding="utf-8") as file:
+    google_api_key = file.read().strip()
+    
+with open(athina_api_key_path, "r", encoding="utf-8") as file:
+    athina_api_key = file.read().strip()
+    
+with open(cleanlab_tlm_api_key_path, "r", encoding="utf-8") as file:
+    cleanlab_tlm_api_key = file.read().strip()
+    
+prompt_template_path = posixpath.join("..", "prompt_templates", "prompt_template_v2.txt")
+
+with open(prompt_template_path, "r", encoding="utf-8") as file:
+    prompt_template = file.read().strip()
+
+schema = gen_and_log_schema_from_jd(jd_path=jd_path,
+                                    scoring_scale_min=1,
+                                    scoring_scale_max=5,
+                                    google_api_key=google_api_key,
+                                    model="gemini-2.5-pro-exp-03-25",
+                                    prompt_template=prompt_template,
+                                    cleanlab_tlm_api_key=cleanlab_tlm_api_key,
+                                    athina_api_key=athina_api_key)
+
+print(schema)
