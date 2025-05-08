9722dca8853037cbaf47337cf71708f63c352c99
daoviethoang1402 <daoviethoang1402@gmail.com>
2025-05-08 12:41:39 +0700

    Connect BE to FE

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import file_router, resume_parser_router, jd_quantifier_router, grader_summarizer_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Cho phép gọi từ front-end
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(file_router.router)
 app.include_router(resume_parser_router.router)
